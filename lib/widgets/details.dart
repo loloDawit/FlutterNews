147ca230cb38045d673d701a7a4a6dfcd7810fee
Dawit Abera <dawitabera64@gmail.com>
2018-09-24 19:34:45 -0700

    news details

---
+++ lib/widgets/details.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../model/newsapiresponse.dart';
+
+class DetailsPage extends StatelessWidget {
+  final Articles article;
+  DetailsPage({this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.bookmark_border),
+        ),
+        body: Scaffold(
+          backgroundColor: Colors.grey,
+          body: ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Hero(
+                      tag: article.urlToImage,
+                      child: Container(
+                        height: 100.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16.0),
+                          shape: BoxShape.rectangle,
+                          image: DecorationImage(
+                              image: NetworkImage("${article.urlToImage}"),
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Text(
+                      article.title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Text(
+                      article.description,
+                      style: Theme.of(context).textTheme.subhead,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
